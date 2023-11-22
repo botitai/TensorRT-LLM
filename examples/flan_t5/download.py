cd1a75d2c74502f02216165b49704230d4cfbda6
nqbao11 <nqbao11.01@gmail.com>
2023-11-22 07:27:45 +0000

    Download model from HF with token

--- examples/flan_t5/download.py
+++ examples/flan_t5/download.py
@@ -2,12 +2,13 @@ import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 import os
 
-model_name = os.environ.get("MODEL_NAME") or "google/flan-t5-xl"
-checkpoint_name = os.environ.get("CHECKPOINT_NAME") or "test"
-# model_name = os.environ.get("MODEL_NAME") or "GotItAI/distilled_elmar_qna_flan_ul2_ft_on_rocketbook_xl"
+MODEL_NAME = os.environ.get("MODEL_NAME")
+CHECKPOINT_NAME = os.environ.get("CHECKPOINT_NAME")
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
-tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=False)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME, token=HF_TOKEN, legacy=False)
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME, token=HF_TOKEN)
 
 input_ids = tokenizer("translate English to German: The house is wonderful.",
                       return_tensors="pt").input_ids
@@ -17,7 +18,7 @@ outputs = model.generate(input_ids, decoder_input_ids=torch.IntTensor([[
 print("input", input_ids, "\noutput", outputs)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
-torch.save(model.state_dict(), f"./models/{checkpoint_name}.ckpt")
+torch.save(model.state_dict(), f"./models/{CHECKPOINT_NAME}.ckpt")
 
 for k, v in model.state_dict().items():
     print(k)
